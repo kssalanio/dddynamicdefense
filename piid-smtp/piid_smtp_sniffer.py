b4c6115f78c9bde4db97a65074cf25f35ee298f1
Ken Salanio <ken.salanio@eee.upd.edu.ph>
2018-12-03 17:14:26 +0800

    proper object inits and thread starters

--- piid-smtp/piid_smtp_sniffer.py
+++ piid-smtp/piid_smtp_sniffer.py
@@ -6,23 +6,28 @@ import time
 
 class SMTPProcessor(object):
     def __init__(self, config):
-        self.packet_buffer_object = sniffer.PacketBuffer(config)
-        self.sniffer = sniffer.PacketSniffer(config, self.packet_buffer_object, app_protocol="smtp")
 
         self.piid_processor = docprocessor.DocumentProcessor(config)
-
+        self.sniffer = sniffer.PacketSniffer(config, self.piid_processor, app_protocol="smtp")
 
     def process_packet_payload(self, packet_payload):
-        self.piid_processor.do_something()
+        smtp_document = packet_payload.split("\n")
 
-        #TODO: detect PII
-        self.piid_processor
+        # Detect PII
+        result = self.piid_processor.detect_smtp(smtp_document)
 
         #TODO: response upon detection
+        if result is True:
+            # Signal to drop
+        else:
+            # Do nothing (?)
+
+    def start(self):
+        self.sniffer.start()
 
 
 if __name__ == '__main__':
     config = ConfigParser.ConfigParser()
     config.read('config.ini')
-    piifilter = PIIFilter(config)
-    piifilter.start()
+    smtp_piifilter = SMTPProcessor(config)
+    smtp_piifilter.start()
