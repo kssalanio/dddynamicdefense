d1873f4cf8a41c6c6155f03af2b9c5baed37e4eb
Ken Salanio <ken.salanio@eee.upd.edu.ph>
2018-12-04 19:13:05 +0800

    added interface for messaging controller for flow drop. also added pii_count

--- piid-smtp/piid_smtp_sniffer.py
+++ piid-smtp/piid_smtp_sniffer.py
@@ -1,5 +1,5 @@
 import ConfigParser
-
+from scapy.all import *
 import sniffer, docprocessor
 from pprint import pprint
 import time
@@ -9,6 +9,14 @@ class SMTPProcessor(object):
 
         self.piid_processor = docprocessor.DocumentProcessor(config)
         self.sniffer = sniffer.PacketSniffer(config, self.piid_processor, app_protocol="smtp")
+        self.pii_count = 0
+
+        # TODO:Fix to interface specced in config.ini
+        active_interface = [intf for intf in get_if_list() if "e" in intf][0]
+        self.piid_MAC = get_if_hwaddr(active_interface)
+        self.piid_IP = os.popen('ip addr show ' + active_interface).read().split("inet ")[1].split("/")[0]
+
+        self.piid_socket = conf.L2socket(iface=active_interface)
 
     def process_packet_payload(self, packet_payload):
         smtp_document = packet_payload.split("\n")
@@ -19,8 +27,9 @@ class SMTPProcessor(object):
         #TODO: response upon detection
         if result is True:
             # Signal to drop
-        else:
-            # Do nothing (?)
+            self.pii_count += 1
+            print("# packets with PII:", self.pii_count, "\n\n")
+            self.piid_socket.send(Ether(src=self.piid_MAC, dst=packet[Ether].dst) / packet[IP])
 
     def start(self):
         self.sniffer.start()
