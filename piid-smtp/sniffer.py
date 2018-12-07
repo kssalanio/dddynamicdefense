a8e7c2f15c66d6e1d2b86cd8a76df160beea2069
Ken Salanio <ken.salanio@eee.upd.edu.ph>
2018-12-07 12:35:22 +0800

    added comments

--- piid-smtp/sniffer.py
+++ piid-smtp/sniffer.py
@@ -10,6 +10,9 @@ from decimal import Decimal
 
 
 class PacketSniffer(threading.Thread):
+    """
+    Packet Sniffer Object using Scapy
+    """
 
     def __init__(self, config, callback_object, app_protocol="smtp"):
         super(PacketSniffer, self).__init__()
@@ -24,23 +27,34 @@ class PacketSniffer(threading.Thread):
         self.callback_object = callback_object
 
     def is_not_outgoing(self, pkt):
-        # return pkt[Ether].src != utils.get_hw_addr(self.ifname)
+        """
+        Filter for ingoing packets
+        :param pkt: Scapy packet object instance
+        :return: True if it is an ingoing packet, False otherwise
+        """
         try:
             return pkt[Ether].src.lower() != get_if_hwaddr(conf.iface).lower()
         except IndexError:
             return False
 
     def is_outgoing(self, pkt):
+        """
+        Filter for outgoing packets
+        :param pkt: Scapy packet object instance
+        :return: True if it is an outgoing packet, False otherwise
+        """
         try:
             return pkt[Ether].src.lower() == get_if_hwaddr(conf.iface).lower()
         except IndexError:
             return False
 
     def sniffer_callback(self, pkt):
+        """
+        Callback passed to Scapy sniff function
+        :param pkt: Scapy packet object instance
+        """
         #if "Ether" in pkt and "IP" in pkt and "TCP" in pkt:
         if "TCP" in pkt:
-            #self.packet_buffer.append(pkt)
-            #TODO: extract payload
 
             # Debug check for packet details
             # print(pkt.summary())
@@ -53,15 +67,20 @@ class PacketSniffer(threading.Thread):
             # else:
             #     print("[NO-LOAD]Packet does not have payload!")
 
-
-
     def print_packet(self, pkt):
+        """
+        Prints out packet src and dst IP
+        :param pkt: Scapy packet object instance
+        """
         ip_layer = pkt.getlayer(IP)
         print("[!] New Packet: {src} -> {dst}".format(src=ip_layer.src, dst=ip_layer.dst))
 
     # New threaded functions
 
     def run(self):
+        """
+        Starts the socket and Scapy sniffer
+        """
         print "Starting Packet Sniffer on [ %s ]:[ %s ]..." % (self.ifname, self.packet_filter_string)
         self.socket = conf.L2listen(
             type=ETH_P_ALL,
@@ -79,8 +98,16 @@ class PacketSniffer(threading.Thread):
         )
 
     def join(self, timeout=None):
+        """
+        Join method for multithreading.
+        :param timeout:
+        """
         self.stop_sniffer.set()
         super(PacketSniffer, self).join(timeout)
 
-    def should_stop_sniffer(self, pkt):
+    def should_stop_sniffer(self):
+        """
+        Stops the sniffer when the internal variable is set
+        :return: True if stop_sniffer is set, False otherwise
+        """
         return self.stop_sniffer.isSet()
