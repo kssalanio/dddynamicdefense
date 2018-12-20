bc497f7702ffb53085d1eead18b9f882da5183e4
Randy Joseph Fernandez <rgfernandez@ieee.org>
2018-12-20 11:51:11 +0000

    removed comment on download link

--- annotation/setup.sh
+++ annotation/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#wget http://weaver.nlplab.org/~brat/releases/brat-v1.3_Crunchy_Frog.tar.gz
+wget http://weaver.nlplab.org/~brat/releases/brat-v1.3_Crunchy_Frog.tar.gz
 
 # extract file
 tar xzf brat-v1.3_Crunchy_Frog.tar.gz
